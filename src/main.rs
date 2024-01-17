8faa96aea34ec07f77c1c0f267745fffa0ee1846
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-01-17 20:29:06 +0100

    add simple lambda

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    let who = event
+        .query_string_parameters_ref()
+        .and_then(|params| params.first("name"))
+        .unwrap_or("world");
+    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(message.into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
